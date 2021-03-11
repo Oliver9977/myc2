4392f65218ca7032f336ceaccc5fc8223d3a0245
Oliver9977 <36608871+Oliver9977@users.noreply.github.com>
2021-03-11 14:41:35 -0500

    payload gen ready ....

--- Client/tools/Invoke-Base64.ps1
+++ Client/tools/Invoke-Base64.ps1
@@ -1,4 +1,4 @@
-$filename = "tools\csexec.exe"
+$filename = "payload\myclient.exe"
 $old_bytes = [IO.File]::ReadAllBytes($filename)
 [system.Convert]::toBase64string($old_bytes) | Clip
 [system.Convert]::toBase64string($old_bytes)

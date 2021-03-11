bf4751d9d47c50cd09f7d55b20e06ac86daf9202
Oliver9977 <36608871+Oliver9977@users.noreply.github.com>
2021-03-10 20:55:24 -0500

    Update localhttpserver.py

--- Server/localhttpserver.py
+++ Server/localhttpserver.py
@@ -6,6 +6,7 @@ for payload transfer with invoke-webrequest, and namepipe
 import http.server
 import socketserver
 import threading
+import uuid
 
 
 
@@ -16,17 +17,26 @@ class localhttpserver():
         self.__local_http_server_port = 80
         self.__local_http_server_path = "HSDB\\"
 
+        self.__local_http_server_uuid_list = list()
+        self.__local_http_server_info_list = dict()
+        self.__local_http_server_httpd_list = dict()
 
-    def __t_start_resource_handler_http_server(self):
+
+    def __t_start_resource_handler_http_server(self,myuuid):
         t_handler = http.server.SimpleHTTPRequestHandler
+        httpd = socketserver.TCPServer((self.__local_http_server_ip, self.__local_http_server_port), t_handler)
+        self.__local_http_server_httpd_list[myuuid] = httpd
 
-        with socketserver.TCPServer((self.__local_http_server_ip, self.__local_http_server_port), t_handler) as httpd:
-            print("serving at port {} ..." .format(self.__local_http_server_port))
-            httpd.serve_forever()
+        print("serving at port {} ..." .format(self.__local_http_server_port))
+        httpd.serve_forever()
 
 
     def start_resource_handler_http_server(self):
-        threading.Thread(target=self.__t_start_resource_handler_http_server).start()
+        myuuid = uuid.uuid4().hex[:6].upper()
+        self.__local_http_server_uuid_list.append(myuuid)
+        self.__local_http_server_info_list[myuuid] = (self.__local_http_server_ip,self.__local_http_server_port,self.__local_http_server_path)
+
+        threading.Thread(target=self.__t_start_resource_handler_http_server,args=(myuuid,)).start()
 
     def set_server_config(self,ip,port,path):
         self.__local_http_server_ip = ip
@@ -39,5 +49,28 @@ class localhttpserver():
         print("Server port: {}".format(self.__local_http_server_port))
         print("Server path: {}".format(self.__local_http_server_path))
 
-        
+    def print_running_server_info(self,myuuid):
+        print("+++++++++++++++++++++Local Http Server Info++++++++++++++++++++++")
+        print("Server UUID: {}".format(myuuid))
+        t_infolist = self.__local_http_server_info_list[myuuid]
+        print("Server ip: {}".format(t_infolist[0]))
+        print("Server port: {}".format(t_infolist[1]))
+        print("Server path: {}".format(t_infolist[2]))
+
+    def get_running_list(self):
+        return self.__local_http_server_uuid_list
+    
+    def stop_resource_handler_http_server(self,myuuid):
+        if uuid in self.__local_http_server_uuid_list:
+            self.__local_http_server_httpd_list[myuuid].shutdown()
+            self.__local_http_server_httpd_list[myuuid].socket.close()
+            self.__local_http_server_httpd_list.pop(myuuid, None)
+            self.__local_http_server_info_list.pop(myuuid,None)
+            self.__local_http_server_uuid_list.remove(myuuid)
+
+        else:
+            print("[HTTPD] not running")
+
+
+
 

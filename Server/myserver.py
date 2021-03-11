ccb50d49042497b7af21fe1a94eb5a1a6dac41b2
Oliver9977 <36608871+Oliver9977@users.noreply.github.com>
2021-03-10 19:49:03 -0500

    Update myserver.py

--- Server/myserver.py
+++ Server/myserver.py
@@ -142,18 +142,6 @@ class mysocket_handler():
 
 
 
-
-# PORT = 8000
-
-# Handler = http.server.SimpleHTTPRequestHandler
-
-# with socketserver.TCPServer(("", PORT), Handler) as httpd:
-#     print("serving at port", PORT)
-#     httpd.serve_forever()
-
-
-
-
 class myserver():
 
     def __init__(self):

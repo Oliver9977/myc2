a7fc1a63df4d1bed189ed3631441232313aebaad
Oliver9977 <36608871+Oliver9977@users.noreply.github.com>
2021-03-11 19:43:13 -0500

    use "Any CPU" for better compatibility ... I hope

--- Client/myclient/build.bat
+++ Client/myclient/build.bat
@@ -1,6 +1,6 @@
 @echo off
-"C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\MSBuild\Current\Bin\MSBuild.exe" /p:Configuration=Release /p:Platform=x64
-copy bin\x64\Release\myclient.exe
-"C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\MSBuild\Current\Bin\MSBuild.exe" /p:Configuration=Release /p:Platform=x64 /t:clean
+"C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\MSBuild\Current\Bin\MSBuild.exe" /p:Configuration=Release /p:Platform="Any CPU"
+copy bin\Release\myclient.exe
+"C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\MSBuild\Current\Bin\MSBuild.exe" /p:Configuration=Release /p:Platform="Any CPU" /t:clean
 rmdir bin /s /q
 rmdir obj /s /q

0725e5eb9d1455201c0d0f446468c7818fc482dc
Oliver9977 <36608871+Oliver9977@users.noreply.github.com>
2021-03-11 20:50:36 -0500

    fix net4.8 ...

--- Client/external/GadgetToJScript/build.bat
+++ Client/external/GadgetToJScript/build.bat
@@ -1,5 +1,5 @@
 @echo off
-GadgetToJScript.NET4.x.exe -c GadgetToJScript.cs -w hta -o test -d System.Linq.dll,System.dll
-GadgetToJScript.NET4.x.exe -c GadgetToJScript.cs -w js -o test -d System.Linq.dll,System.dll
-GadgetToJScript.NET4.x.exe -c GadgetToJScript.cs -w vba -o test -d System.Linq.dll,System.dll
-GadgetToJScript.NET4.x.exe -c GadgetToJScript.cs -w vbs -o test -d System.Linq.dll,System.dll
+GadgetToJScript.NET4.x.exe -c GadgetToJScript.cs -w hta -o test -d System.Linq.dll,System.dll --bypass
+GadgetToJScript.NET4.x.exe -c GadgetToJScript.cs -w js -o test -d System.Linq.dll,System.dll --bypass
+GadgetToJScript.NET4.x.exe -c GadgetToJScript.cs -w vba -o test -d System.Linq.dll,System.dll --bypass
+GadgetToJScript.NET4.x.exe -c GadgetToJScript.cs -w vbs -o test -d System.Linq.dll,System.dll --bypass

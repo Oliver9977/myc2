382342358dc92a6b4f025ea1e7377841eaccdc96
Oliver9977 <36608871+Oliver9977@users.noreply.github.com>
2021-03-12 08:58:26 -0500

    add x86 for the payload ...

---
+++ Client/myclient/build32.bat
@@ -0,0 +1,6 @@
+@echo off
+"C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\MSBuild\Current\Bin\MSBuild.exe" /p:Configuration=Release /p:Platform="x86"
+copy bin\x86\Release\myclient.exe myclientx86.exe
+"C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\MSBuild\Current\Bin\MSBuild.exe" /p:Configuration=Release /p:Platform="x86" /t:clean
+rmdir bin /s /q
+rmdir obj /s /q

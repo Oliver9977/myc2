b73b7b5efc3a22f20ec5109599f854971cd0aa3b
Oliver9977 <36608871+Oliver9977@users.noreply.github.com>
2021-03-21 02:11:15 -0400

    add encoder ..

--- Server/decoder.py
+++ Server/decoder.py
@@ -3,9 +3,12 @@ import base64
 def isBase64(s): #not a good idea .. but should work ... cannot be a "single word"
     try:
         #print(base64.b64decode(s))
-        return base64.b64encode(base64.b64decode(s)).decode("utf-8") == s
+        return base64.b64encode(base64.b64decode(s)).decode("utf-8","ignore") == s
     except:
         return False
     
 def b64_decode(s):
     return base64.b64decode(s)
+
+def b64_encode(s):
+    return base64.b64encode(s.encode("utf-8","ignore")).decode("utf-8","ignore")

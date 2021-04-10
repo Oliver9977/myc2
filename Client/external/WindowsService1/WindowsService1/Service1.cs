b9d137502d80d50a802844746b64c4a1bac3cbdb
Oliver9977 <36608871+Oliver9977@users.noreply.github.com>
2021-04-10 13:39:32 -0400

    Added Service ..

---
+++ Client/external/WindowsService1/WindowsService1/Service1.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Linq;
+using System.ServiceProcess;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace WindowsService1
+{
+    public partial class Service1 : ServiceBase
+    {
+        public Service1()
+        {
+            InitializeComponent();
+        }
+
+        protected override void OnStart(string[] args)
+        {
+            var si = new ProcessStartInfo
+            {
+                FileName = @"C:\windows\temp\test.exe",
+                Arguments = @"dummy"
+            };
+
+            var proc = new Process
+            {
+                StartInfo = si
+            };
+
+            var t = new Thread(() =>
+            {
+                proc.Start();
+                proc.WaitForExit();
+                proc.Dispose();
+            });
+
+            t.Start();
+        }
+
+        protected override void OnStop()
+        {
+            base.OnStop();
+        }
+    }
+}

706c57892002000baa4a22faa2f83ae3f18b277e
Oliver9977 <36608871+Oliver9977@users.noreply.github.com>
2021-04-13 19:51:56 -0400

    Create Impersonator.cs

---
+++ Client/myclient/Impersonator.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Security.Principal;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace myclient
+{
+    class Impersonator
+
+    {
+
+        [DllImport("advapi32.dll", SetLastError = true)]
+        private static extern int LogonUser(
+            string lpszUserName,
+            string lpszDomain,
+            string lpszPassword,
+            int dwLogonType,
+            int dwLogonProvider,
+            ref IntPtr phToken);
+
+        [DllImport("advapi32.dll", CharSet = CharSet.Auto, SetLastError = true)]
+        private static extern int DuplicateToken(
+            IntPtr hToken,
+            int impersonationLevel,
+            ref IntPtr hNewToken);
+
+        [DllImport("advapi32.dll", CharSet = CharSet.Auto, SetLastError = true)]
+        private static extern bool RevertToSelf();
+
+        [DllImport("kernel32.dll", CharSet = CharSet.Auto)]
+        private static extern bool CloseHandle(
+            IntPtr handle);
+
+        private const int LOGON32_LOGON_INTERACTIVE = 2;
+        private const int LOGON32_LOGON_NEW_CREDENTIALS = 9;
+        private const int LOGON32_PROVIDER_DEFAULT = 0;
+
+
+        public void ImpersonateValidUser(
+            string userName,
+            string domain,
+            string password)
+        {
+            WindowsIdentity tempWindowsIdentity = null;
+            IntPtr token = IntPtr.Zero;
+            IntPtr tokenDuplicate = IntPtr.Zero;
+
+            try
+            {
+                if (RevertToSelf())
+                {
+                    if (LogonUser(userName, domain, password, LOGON32_LOGON_NEW_CREDENTIALS, LOGON32_PROVIDER_DEFAULT, ref token) != 0)
+                    {
+                        if (DuplicateToken(token, 2, ref tokenDuplicate) != 0)
+                        {
+                            tempWindowsIdentity = new WindowsIdentity(tokenDuplicate);
+                            impersonationContext = tempWindowsIdentity.Impersonate();
+                        }
+                        else
+                        {
+                            Console.WriteLine("DuplicateToken Failed");
+                        }
+                    }
+                    else
+                    {
+                        Console.WriteLine("LogonUser Failed");
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("RevertToSelf Failed");
+                }
+            }
+
+            finally
+            {
+                if (token != IntPtr.Zero)
+                {
+                    CloseHandle(token);
+                }
+                if (tokenDuplicate != IntPtr.Zero)
+                {
+                    CloseHandle(tokenDuplicate);
+                }
+            }
+        }
+
+
+        public void UndoImpersonation()
+        {
+            if (impersonationContext != null)
+            {
+                impersonationContext.Undo();
+            }
+        }
+
+        private WindowsImpersonationContext impersonationContext = null;
+
+    }
+}
